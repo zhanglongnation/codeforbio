2835d14c01c9c8acee447656925e695005e16357
zhanglongnation <133081072+zhanglongnation@users.noreply.github.com>
2023-05-10 12:28:57 +0800

    Create remove_repeat_from_genome.py

---
+++ remove_repeat_from_genome.py
@@ -0,0 +1,14 @@
+import re
+filein=open("TGS_pol3.fasta.masked_trans",'r')
+for line in filein:
+    if line.startswith(">"):
+        name=line[1:]
+    else:
+        a=[substr.start() for substr in re.finditer("[A-Z]+",line)]
+        b=[substr.end() for substr in re.finditer("[A-Z]+",line)]
+        length=len(a)
+        for i in range(0,length):
+            if b[i] -a[i] > 10:
+                print(">"+name.strip()+":"+str(a[i]+1)+"-"+str(b[i]))
+                print(line[a[i]:b[i]])
+filein.close()
